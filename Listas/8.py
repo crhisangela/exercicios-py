bb55bcc5595d08dd06574ce58c8714d8f0bc3708
Crhis Ferreira <ferreiracrhis@gmail.com>
2022-04-16 09:45:17 -0300

    8

---
+++ Listas/8.py
@@ -0,0 +1,18 @@
+#8. Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo vetor. Imprima a idade e a altura na ordem inversa a ordem lida
+
+
+idade = []
+altura = []
+
+for valor in range (0,5):
+    idade.append(int(input("Digite a idade: ")))
+    altura.append(float(input("Digite a altura: ")))
+
+print('Ordem inversa')
+for valor in range(0,5):
+    print(idade[len(idade)-1-valor])
+
+
+print("Alturas na ordem inversa: ")
+for valor in range(0, 5):
+    print(altura[len(altura)-1-valor])
